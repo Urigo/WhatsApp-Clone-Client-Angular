2a0c31bf6beae3ad6b6e4325159945e0b3a39e64
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-12-06 19:41:57 +0100

    Step 1.3: List the chats

---
+++ src/app/chats-lister/chats-lister.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatIconModule, MatListModule, MatMenuModule} from '@angular/material';
+import {RouterModule, Routes} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {ChatsService} from '../services/chats.service';
+import {ChatItemComponent} from './components/chat-item/chat-item.component';
+import {ChatsComponent} from './containers/chats/chats.component';
+import {ChatsListComponent} from './components/chats-list/chats-list.component';
+import {SharedModule} from '../shared/shared.module';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'chats', pathMatch: 'full'},
+  {path: 'chats', component: ChatsComponent},
+];
+
+@NgModule({
+  declarations: [
+    ChatsComponent,
+    ChatsListComponent,
+    ChatItemComponent,
+  ],
+  imports: [
+    BrowserModule,
+    // Material
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    // Animations
+    BrowserAnimationsModule,
+    // Routing
+    RouterModule.forChild(routes),
+    // Forms
+    FormsModule,
+    // Feature modules
+    SharedModule,
+  ],
+  providers: [
+    ChatsService,
+  ],
+})
+export class ChatsListerModule {
+}
