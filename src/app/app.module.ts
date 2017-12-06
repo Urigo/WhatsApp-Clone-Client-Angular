0681432dbf7aee40380c148dd183a6d26dfe0d3f
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-12-06 17:42:00 +0100

    Step 1.1: Add angular-apollo to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {HttpLink, HttpLinkModule, Options} from 'apollo-angular-link-http';
+import {Apollo, ApolloModule} from 'apollo-angular';
+import {InMemoryCache} from 'apollo-cache-inmemory';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // Apollo
+    ApolloModule,
+    HttpLinkModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    apollo: Apollo,
+    httpLink: HttpLink,
+  ) {
+    apollo.create({
+      link: httpLink.create(<Options>{uri: 'http://localhost:3000/graphql'}),
+      cache: new InMemoryCache(),
+    });
+  }
+}
