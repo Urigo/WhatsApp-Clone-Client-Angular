e202e86ce90940a8bedd45b436f6eee4e4d825d5
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-12-06 19:41:57 +0100

    Step 1.3: List the chats

---
+++ src/app/chats-lister/components/chat-item/chat-item.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-chat-item',
+  template: `
+    <div class="chat-row">
+        <div class="chat-recipient">
+          <img *ngIf="chat.picture" [src]="chat.picture" width="48" height="48">
+          <div>{{ chat.name }} [id: {{ chat.id }}]</div>
+        </div>
+        <div class="chat-content">{{ chat.messages[chat.messages.length - 1]?.content | truncate : 20 : '...' }}</div>
+    </div>
+  `,
+  styleUrls: ['chat-item.component.scss'],
+})
+export class ChatItemComponent {
+  // tslint:disable-next-line:no-input-rename
+  @Input('item')
+  chat: any;
+}
