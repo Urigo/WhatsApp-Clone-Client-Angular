3c7247068c6fb4fbb0aa4c67e7120704545e3f09
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-12-06 17:49:52 +0100

    Step 1.2: Add chats service

---
+++ src/app/services/chats.service.ts
@@ -0,0 +1,25 @@
+import {map} from 'rxjs/operators';
+import {Apollo} from 'apollo-angular';
+import {Injectable} from '@angular/core';
+import {getChatsQuery} from '../../graphql/getChats.query';
+
+@Injectable()
+export class ChatsService {
+  messagesAmount = 3;
+
+  constructor(private apollo: Apollo) {}
+
+  getChats() {
+    const query = this.apollo.watchQuery<any>({
+      query: getChatsQuery,
+      variables: {
+        amount: this.messagesAmount,
+      },
+    });
+    const chats$ = query.valueChanges.pipe(
+      map((result) => result.data.chats)
+    );
+
+    return {query, chats$};
+  }
+}
