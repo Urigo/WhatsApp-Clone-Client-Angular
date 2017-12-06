b3e19f86091778e28f057032d3da6a1f469d45b7
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-12-06 19:41:57 +0100

    Step 1.3: List the chats

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
---
+++ src/app/shared/components/toolbar/toolbar.component.ts
@@ -0,0 +1,19 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar',
+  template: `
+    <mat-toolbar>
+      <div class="left-block">
+        <ng-content select=".navigation"></ng-content>
+        <ng-content select=".profile-pic"></ng-content>
+        <ng-content select=".title"></ng-content>
+      </div>
+      <ng-content select=".menu"></ng-content>
+    </mat-toolbar>
+  `,
+  styleUrls: ['./toolbar.component.scss']
+})
+export class ToolbarComponent {
+
+}
