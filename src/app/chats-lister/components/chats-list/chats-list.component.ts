125eab7f40291443f2eae223d0307088ad792374
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-12-06 19:41:57 +0100

    Step 1.3: List the chats

---
+++ src/app/chats-lister/components/chats-list/chats-list.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-chats-list',
+  template: `
+    <mat-list>
+      <mat-list-item *ngFor="let chat of chats">
+        <app-chat-item [item]="chat"></app-chat-item>
+      </mat-list-item>
+    </mat-list>
+  `,
+  styleUrls: ['chats-list.component.scss'],
+})
+export class ChatsListComponent {
+  // tslint:disable-next-line:no-input-rename
+  @Input('items')
+  chats: any[];
+
+  constructor() {}
+}
