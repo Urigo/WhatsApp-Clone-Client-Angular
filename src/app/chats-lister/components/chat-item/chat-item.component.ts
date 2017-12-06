63ebdd81f45056eb2b152ce334b10f0bcbd94a4d
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-12-06 19:41:57 +0100

    Step 1.3: List the chats

---
+++ src/app/chats-lister/components/chat-item/chat-item.component.ts
@@ -0,0 +1,29 @@
+import {Component, Input} from '@angular/core';
+import * as moment from 'moment';
+
+@Component({
+  selector: 'app-chat-item',
+  template: `
+    <div class="chat-row">
+      <img class="chat-pic" [src]="chat.picture || 'assets/default-profile-pic.jpg'">
+      <div class="chat-info">
+        <div class="chat-name">{{ chat.name }}</div>
+        <div class="chat-last-message">{{ chat.messages[chat.messages.length - 1]?.content }}</div>
+        <div class="chat-timestamp">{{ updatedAt }}</div>
+      </div>
+    </div>
+  `,
+  styleUrls: ['chat-item.component.scss'],
+})
+export class ChatItemComponent {
+  // tslint:disable-next-line:no-input-rename
+  @Input('item')
+  chat: any;
+
+  updatedAt: string;
+
+  ngOnInit() {
+    this.updatedAt = this.chat.updatedAt
+      ? moment(this.chat.updatedAt).format('HH:mm') : '';
+  }
+}
