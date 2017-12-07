95c1bc4023243f3829a50b3deb66a1414094fc67
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-12-07 11:31:46 +0100

    Step 4.1: Chat Viewer

--- src/app/chats-lister/components/chat-item/chat-item.component.ts
+++ src/app/chats-lister/components/chat-item/chat-item.component.ts
@@ -1,14 +1,14 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {GetChats} from '../../../../graphql';
 
 @Component({
   selector: 'app-chat-item',
   template: `
-    <div class="chat-row">
+    <div class="chat-row" (click)="selectChat()">
       <img class="chat-pic" [src]="chat.picture || 'assets/default-profile-pic.jpg'">
       <div class="chat-info">
         <div class="chat-name">{{ chat.name }}</div>
-        <div class="chat-last-message">{{ chat.messages?[chat.messages?.length - 1]?.content }}</div>
+        <div class="chat-last-message">{{ chat.messages[chat.messages.length - 1]?.content }}</div>
         <div class="chat-timestamp">00:00</div>
       </div>
     </div>
@@ -19,4 +19,11 @@ export class ChatItemComponent {
   // tslint:disable-next-line:no-input-rename
   @Input('item')
   chat: GetChats.Chats;
+
+  @Output()
+  select = new EventEmitter<string>();
+
+  selectChat() {
+    this.select.emit(this.chat.id);
+  }
 }
