48ec6d095443dfde22ae2501f9780f33ce1c677f
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-12-07 10:22:14 +0100

    Step 2.2: Run generator

---
+++ src/graphql.ts
@@ -0,0 +1,134 @@
+export type Maybe<T> = T | null;
+
+export enum MessageType {
+  Location = "LOCATION",
+  Text = "TEXT",
+  Picture = "PICTURE"
+}
+
+/** A date-time string at UTC, such as 2007-12-03T10:15:30Z, compliant with the `date-time` format outlined in section 5.6 of the RFC 3339 profile of the ISO 8601 standard for representation of dates and times using the Gregorian calendar. */
+export type DateTime = any;
+
+// ====================================================
+// Documents
+// ====================================================
+
+export namespace GetChats {
+  export type Variables = {
+    amount?: Maybe<number>;
+  };
+
+  export type Query = {
+    __typename?: "Query";
+
+    chats: Maybe<Chats[]>;
+  };
+
+  export type Chats = {
+    __typename?: "Chat";
+
+    messages: (Maybe<Messages>)[];
+  } & ChatWithoutMessages.Fragment;
+
+  export type Messages = Message.Fragment;
+}
+
+export namespace ChatWithoutMessages {
+  export type Fragment = {
+    __typename?: "Chat";
+
+    id: string;
+
+    name: Maybe<string>;
+
+    picture: Maybe<string>;
+
+    allTimeMembers: AllTimeMembers[];
+
+    unreadMessages: number;
+
+    isGroup: boolean;
+  };
+
+  export type AllTimeMembers = {
+    __typename?: "User";
+
+    id: string;
+  };
+}
+
+export namespace Message {
+  export type Fragment = {
+    __typename?: "Message";
+
+    id: string;
+
+    chat: Chat;
+
+    sender: Sender;
+
+    content: string;
+
+    createdAt: DateTime;
+
+    type: number;
+
+    recipients: Recipients[];
+
+    ownership: boolean;
+  };
+
+  export type Chat = {
+    __typename?: "Chat";
+
+    id: string;
+  };
+
+  export type Sender = {
+    __typename?: "User";
+
+    id: string;
+
+    name: Maybe<string>;
+  };
+
+  export type Recipients = {
+    __typename?: "Recipient";
+
+    user: User;
+
+    message: Message;
+
+    chat: __Chat;
+
+    receivedAt: Maybe<DateTime>;
+
+    readAt: Maybe<DateTime>;
+  };
+
+  export type User = {
+    __typename?: "User";
+
+    id: string;
+  };
+
+  export type Message = {
+    __typename?: "Message";
+
+    id: string;
+
+    chat: _Chat;
+  };
+
+  export type _Chat = {
+    __typename?: "Chat";
+
+    id: string;
+  };
+
+  export type __Chat = {
+    __typename?: "Chat";
+
+    id: string;
+  };
+}
