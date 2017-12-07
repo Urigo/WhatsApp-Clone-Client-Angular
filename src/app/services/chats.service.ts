764200d05e4ab03c14870dd76c154c102536ddd2
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-12-07 12:55:02 +0100

    Step 6.1: Update the store

--- src/app/services/chats.service.ts
+++ src/app/services/chats.service.ts
@@ -1,6 +1,13 @@
 import {map} from 'rxjs/operators';
 import {Injectable} from '@angular/core';
-import {GetChatsGQL, GetChatGQL, AddMessageGQL} from '../../graphql';
+import {
+  GetChatsGQL,
+  GetChatGQL,
+  AddMessageGQL,
+  AddMessage,
+  GetChats,
+  GetChat
+} from '../../graphql';
 
 @Injectable()
 export class ChatsService {
@@ -39,6 +46,50 @@ export class ChatsService {
     return this.addMessageGQL.mutate({
       chatId,
       content,
+    }, {
+      update: (store, { data: { addMessage } }: {data: AddMessage.Mutation}) => {
+        // Update the messages cache
+        {
+          // Read the data from our cache for this query.
+          const {chat} = store.readQuery<GetChat.Query, GetChat.Variables>({
+            query: this.getChatGQL.document,
+            variables: {
+              chatId,
+            }
+          });
+          // Add our message from the mutation to the end.
+          chat.messages.push(addMessage);
+          // Write our data back to the cache.
+          store.writeQuery({
+            query: this.getChatGQL.document,
+            data: {
+              chat
+            }
+          });
+        }
+        // Update last message cache
+        {
+          // Read the data from our cache for this query.
+          const {chats} = store.readQuery<GetChats.Query, GetChats.Variables>({
+            query: this.getChatsGQL.document,
+            variables: {
+              amount: this.messagesAmount,
+            },
+          });
+          // Add our comment from the mutation to the end.
+          chats.find(chat => chat.id === chatId).messages.push(addMessage);
+          // Write our data back to the cache.
+          store.writeQuery<GetChats.Query, GetChats.Variables>({
+            query: this.getChatsGQL.document,
+            variables: {
+              amount: this.messagesAmount,
+            },
+            data: {
+              chats,
+            },
+          });
+        }
+      }
     });
   }
 }
