ad768516d9d2fae34341a88a5e6955e86e544896
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-12-08 00:16:41 +0100

    Step 7.2: Remove messages and chats

--- src/types.d.ts
+++ src/types.d.ts
@@ -225,3 +225,32 @@ export namespace GetChats {
     readAt?: number | null; 
   } 
 }
+export namespace RemoveAllMessages {
+  export type Variables = {
+    chatId: string;
+    all?: boolean | null;
+  }
+
+  export type Mutation = {
+    removeMessages?: string[] | null; 
+  } 
+}
+export namespace RemoveChat {
+  export type Variables = {
+    chatId: string;
+  }
+
+  export type Mutation = {
+    removeChat?: string | null; 
+  } 
+}
+export namespace RemoveMessages {
+  export type Variables = {
+    chatId: string;
+    messageIds?: string[] | null;
+  }
+
+  export type Mutation = {
+    removeMessages?: string[] | null; 
+  } 
+}
