094e247755dd2fe362705d9d21a57034fcb0cc3d
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-12-07 12:55:02 +0100

    Step 6.1: Update the store

--- src/app/services/chats.service.ts
+++ src/app/services/chats.service.ts
@@ -35,18 +35,55 @@ export class ChatsService {
     });
 
     const chat$ = query.valueChanges.pipe(
-      map((result: ApolloQueryResult<GetChat.Query>) => result.data.chat)
+      map((result) => result.data.chat)
     );
 
     return {query, chat$};
   }
 
   addMessage(chatId: string, content: string) {
-    return this.apollo.mutate({
+    return this.apollo.mutate<AddMessage.Mutation, AddMessage.Variables>({
       mutation: addMessageMutation,
-      variables: <AddMessage.Variables>{
+      variables: {
         chatId,
         content,
+      },
+      update: (store, { data: { addMessage } }: {data: AddMessage.Mutation}) => {
+        // Update the messages cache
+        {
+          // Read the data from our cache for this query.
+          const {chat} = store.readQuery<GetChat.Query, GetChat.Variables>({
+            query: getChatQuery, variables: {
+              chatId,
+            }
+          });
+          // Add our message from the mutation to the end.
+          chat.messages.push(addMessage);
+          // Write our data back to the cache.
+          store.writeQuery({ query: getChatQuery, data: {chat} });
+        }
+        // Update last message cache
+        {
+          // Read the data from our cache for this query.
+          const {chats} = store.readQuery<GetChats.Query, GetChats.Variables>({
+            query: getChatsQuery,
+            variables: {
+              amount: this.messagesAmount,
+            },
+          });
+          // Add our comment from the mutation to the end.
+          chats.find(chat => chat.id === chatId).messages.push(addMessage);
+          // Write our data back to the cache.
+          store.writeQuery<GetChats.Query, GetChats.Variables>({
+            query: getChatsQuery,
+            variables: {
+              amount: this.messagesAmount,
+            },
+            data: {
+              chats,
+            },
+          });
+        }
       },
     });
   }
