ecff66f1518a0cb733b0bab21d99742dead74cdf
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-12-08 00:16:41 +0100

    Step 7.2: Remove messages and chats

--- src/app/services/chats.service.ts
+++ src/app/services/chats.service.ts
@@ -4,10 +4,16 @@ import {
   GetChatsGQL,
   GetChatGQL,
   AddMessageGQL,
+  RemoveChatGQL,
+  RemoveMessagesGQL,
+  RemoveAllMessagesGQL,
   AddMessage,
   GetChats,
-  GetChat
+  GetChat,
+  RemoveMessages,
+  RemoveAllMessages,
 } from '../../graphql';
+import { DataProxy } from 'apollo-cache';
 
 @Injectable()
 export class ChatsService {
@@ -16,7 +22,10 @@ export class ChatsService {
   constructor(
     private getChatsGQL: GetChatsGQL,
     private getChatGQL: GetChatGQL,
-    private addMessageGQL: AddMessageGQL
+    private addMessageGQL: AddMessageGQL,
+    private removeChatGQL: RemoveChatGQL,
+    private removeMessagesGQL: RemoveMessagesGQL,
+    private removeAllMessagesGQL: RemoveAllMessagesGQL,
   ) {}
 
   getChats() {
@@ -91,5 +100,112 @@ export class ChatsService {
         }
       }
     });
+  }
+
+  removeChat(chatId: string) {
+    return this.removeChatGQL.mutate(
+      {
+        chatId,
+      }, {
+        update: (store, { data: { removeChat } }) => {
+          // Read the data from our cache for this query.
+          const {chats} = store.readQuery<GetChats.Query, GetChats.Variables>({
+            query: this.getChatsGQL.document,
+            variables: {
+              amount: this.messagesAmount,
+            },
+          });
+          // Remove the chat (mutable)
+          for (const index of chats.keys()) {
+            if (chats[index].id === removeChat) {
+              chats.splice(index, 1);
+            }
+          }
+          // Write our data back to the cache.
+          store.writeQuery<GetChats.Query, GetChats.Variables>({
+            query: this.getChatsGQL.document,
+            variables: {
+              amount: this.messagesAmount,
+            },
+            data: {
+              chats,
+            },
+          });
+        },
+      }
+    );
+  }
+
+  removeMessages(chatId: string, messages: GetChat.Messages[], messageIdsOrAll: string[] | boolean) {
+    let ids: string[] = [];
+
+    const options = {
+      update: (store: DataProxy, { data: { removeMessages } }: {data: RemoveMessages.Mutation | RemoveAllMessages.Mutation}) => {
+        // Update the messages cache
+        {
+          // Read the data from our cache for this query.
+          const {chat} = store.readQuery<GetChat.Query, GetChat.Variables>({
+            query: this.getChatGQL.document,
+            variables: {
+              chatId,
+            }
+          });
+          // Remove the messages (mutable)
+          removeMessages.forEach(messageId => {
+            for (const index of chat.messages.keys()) {
+              if (chat.messages[index].id === messageId) {
+                chat.messages.splice(index, 1);
+              }
+            }
+          });
+          // Write our data back to the cache.
+          store.writeQuery<GetChat.Query, GetChat.Variables>({
+            query: this.getChatGQL.document,
+            data: {
+              chat
+            }
+          });
+        }
+        // Update last message cache
+        {
+          // Read the data from our cache for this query.
+          const {chats} = store.readQuery<GetChats.Query, GetChats.Variables>({
+            query: this.getChatsGQL.document,
+            variables: {
+              amount: this.messagesAmount,
+            },
+          });
+          // Fix last message
+          chats.find(chat => chat.id === chatId).messages = messages
+            .filter(message => !ids.includes(message.id));
+          // Write our data back to the cache.
+          store.writeQuery<GetChats.Query, GetChats.Variables>({
+            query: this.getChatsGQL.document,
+            variables: {
+              amount: this.messagesAmount,
+            },
+            data: {
+              chats,
+            },
+          });
+        }
+      }
+    };
+
+    if (typeof messageIdsOrAll === 'boolean') {
+      ids = messages.map(message => message.id);
+
+      return this.removeAllMessagesGQL.mutate({
+        chatId,
+        all: messageIdsOrAll
+      }, options);
+    } else {
+      ids = messageIdsOrAll;
+
+      return this.removeMessagesGQL.mutate({
+        chatId,
+        messageIds: messageIdsOrAll,
+      }, options);
+    }
   }
 }
