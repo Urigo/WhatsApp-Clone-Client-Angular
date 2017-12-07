1b33cf7e1bd3295b4553b92f0ddacf361f93e927
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-12-07 12:55:02 +0100

    Step 6.1: Update the store

--- src/app/services/chats.service.ts
+++ src/app/services/chats.service.ts
@@ -1,6 +1,13 @@
 import {map} from 'rxjs/operators';
 import {Injectable} from '@angular/core';
-import {GetChatsGQL, GetChatGQL, AddMessageGQL} from '../../graphql';
+import {
+  GetChatsGQL,
+  GetChatGQL,
+  AddMessageGQL,
+  AddMessage,
+  GetChats,
+  GetChat
+} from '../../graphql';
 
 @Injectable()
 export class ChatsService {
@@ -39,6 +46,58 @@ export class ChatsService {
     return this.addMessageGQL.mutate({
       chatId,
       content,
+    }, {
+      update: (store, { data: { addMessage } }: {data: AddMessage.Mutation}) => {
+        // Update the messages cache
+        {
+          // Read the data from our cache for this query.
+          const {chat} = store.readQuery<GetChat.Query, GetChat.Variables>({
+            query: this.getChatGQL.document,
+            variables: {
+              chatId,
+            }
+          });
+          if (!chat.messages.some(message => message.id === addMessage.id)) {
+            // Add our message from the mutation to the end.
+            chat.messages.push(addMessage);
+            // Write our data back to the cache.
+            store.writeQuery({
+              query: this.getChatGQL.document,
+              data: {
+                chat
+              }
+            });
+          }
+        }
+        // Update last message cache
+        {
+          // Read the data from our cache for this query.
+          const {chats} = store.readQuery<GetChats.Query, GetChats.Variables>({
+            query: this.getChatsGQL.document,
+            variables: {
+              amount: this.messagesAmount,
+            },
+          });
+          const chat = chats.find(chat => chat.id === chatId);
+          // Add our comment from the mutation to the end.
+          if (!chat.messages.some(message => message.id === addMessage.id)) {
+            // Move chat to the top of the list
+            const index = chats.findIndex(candi => candi.id === chat.id)
+            chats.splice(index, 1)
+            chats.unshift(chat)
+            // Write our data back to the cache.
+            store.writeQuery<GetChats.Query, GetChats.Variables>({
+              query: this.getChatsGQL.document,
+              variables: {
+                amount: this.messagesAmount,
+              },
+              data: {
+                chats,
+              },
+            });
+          }
+        }
+      }
     });
   }
 }
