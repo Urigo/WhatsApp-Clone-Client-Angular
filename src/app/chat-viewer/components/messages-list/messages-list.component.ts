1ebcfe87712a02aa00366f9259afea3d76af9f50
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-12-08 00:16:41 +0100

    Step 7.2: Remove messages and chats

--- src/app/chat-viewer/components/messages-list/messages-list.component.ts
+++ src/app/chat-viewer/components/messages-list/messages-list.component.ts
@@ -1,14 +1,17 @@
 import {Component, Input} from '@angular/core';
 import {GetChat} from '../../../../graphql';
+import {SelectableListDirective} from 'ngx-selectable-list';
 
 @Component({
   selector: 'app-messages-list',
   template: `
     <mat-list>
       <mat-list-item *ngFor="let message of messages">
-        <app-message-item [item]="message" [isGroup]="isGroup"></app-message-item>
+        <app-message-item [item]="message" [isGroup]="isGroup"
+                          libSelectableItem></app-message-item>
       </mat-list-item>
     </mat-list>
+    <ng-content *ngIf="selectableListDirective.selecting"></ng-content>
   `,
   styleUrls: ['messages-list.component.scss'],
 })
@@ -20,5 +23,5 @@ export class MessagesListComponent {
   @Input()
   isGroup: boolean;
 
-  constructor() {}
+  constructor(public selectableListDirective: SelectableListDirective) {}
 }
