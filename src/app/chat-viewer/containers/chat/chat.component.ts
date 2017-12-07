8cdf70b2c0c56a9391bdd0263e2f87fe1ab6bb4a
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-12-07 11:31:46 +0100

    Step 4.1: Chat Viewer

---
+++ src/app/chat-viewer/containers/chat/chat.component.ts
@@ -0,0 +1,46 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {ChatsService} from '../../../services/chats.service';
+
+@Component({
+  template: `
+    <app-toolbar>
+      <button class="navigation" mat-button (click)="goToChats()">
+        <mat-icon aria-label="Icon-button with an arrow back icon">arrow_back</mat-icon>
+      </button>
+      <img class="profile-pic" src="assets/default-profile-pic.jpg">
+      <div class="title">{{ name }}</div>
+    </app-toolbar>
+    <div class="container">
+      <app-messages-list [items]="messages" [isGroup]="isGroup"></app-messages-list>
+      <app-new-message></app-new-message>
+    </div>
+  `,
+  styleUrls: ['./chat.component.scss']
+})
+export class ChatComponent implements OnInit {
+  chatId: string;
+  messages: any[];
+  name: string;
+  isGroup: boolean;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private chatsService: ChatsService) {
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe(({id: chatId}) => {
+      this.chatId = chatId;
+      this.chatsService.getChat(chatId).chat$.subscribe(chat => {
+        this.messages = chat.messages;
+        this.name = chat.name;
+        this.isGroup = chat.isGroup;
+      });
+    });
+  }
+
+  goToChats() {
+    this.router.navigate(['/chats']);
+  }
+}
