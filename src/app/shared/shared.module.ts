a7d7bec05efcbe1f30fd9573821ff03206717d93
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-12-08 00:16:41 +0100

    Step 7.2: Remove messages and chats

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,19 +1,23 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
-import {MatToolbarModule} from '@angular/material';
+import {MatButtonModule, MatIconModule, MatToolbarModule} from '@angular/material';
 import {ToolbarComponent} from './components/toolbar/toolbar.component';
 import {FormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ConfirmSelectionComponent} from './components/confirm-selection/confirm-selection.component';
 
 @NgModule({
   declarations: [
     ToolbarComponent,
+    ConfirmSelectionComponent,
   ],
   imports: [
     BrowserModule,
     // Material
     MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
     // Animations
     BrowserAnimationsModule,
     // Forms
@@ -22,6 +26,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
   providers: [],
   exports: [
     ToolbarComponent,
+    ConfirmSelectionComponent,
   ],
 })
 export class SharedModule {
