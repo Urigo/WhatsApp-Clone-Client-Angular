d60892f63f8d5b9fac8f38a10dda12a768c15764
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-12-07 10:29:22 +0100

    Step 2.3: Use the generated types

--- src/app/services/chats.service.ts
+++ src/app/services/chats.service.ts
@@ -3,6 +3,7 @@ import {map} from 'rxjs/operators';
 import {Apollo} from 'apollo-angular';
 import {Injectable} from '@angular/core';
 import {getChatsQuery} from '../../graphql/getChats.query';
+import {GetChats} from '../../types';
 
 @Injectable()
 export class ChatsService {
@@ -11,14 +12,14 @@ export class ChatsService {
   constructor(private apollo: Apollo) {}
 
   getChats() {
-    const query = this.apollo.watchQuery<any>(<WatchQueryOptions>{
+    const query = this.apollo.watchQuery<GetChats.Query>(<WatchQueryOptions>{
       query: getChatsQuery,
       variables: {
         amount: this.messagesAmount,
       },
     });
     const chats$ = query.valueChanges.pipe(
-      map((result: ApolloQueryResult<any>) => result.data.chats)
+      map((result: ApolloQueryResult<GetChats.Query>) => result.data.chats)
     );
 
     return {query, chats$};
