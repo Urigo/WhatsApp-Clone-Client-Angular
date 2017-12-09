1068794ba8e3624c4caeb6eeca9dc0b44f10c49a
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-12-09 18:30:01 +0100

    Step 9.3: Landing page for new chats/groups

--- src/app/chat-viewer/containers/chat/chat.component.spec.ts
+++ src/app/chat-viewer/containers/chat/chat.component.spec.ts
@@ -129,7 +129,10 @@ describe('ChatComponent', () => {
         },
         {
           provide: ActivatedRoute,
-          useValue: { params: of({ id: chat.id }) },
+          useValue: {
+            params: of({ id: chat.id }),
+            queryParams: of({}),
+          },
         },
       ],
       schemas: [NO_ERRORS_SCHEMA],
