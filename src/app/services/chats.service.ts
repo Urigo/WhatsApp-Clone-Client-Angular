288bfbb1f486ab2b9750ed6eb6bdcabe8b4a766f
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-12-09 18:00:02 +0100

    Step 9.2: Get chat data from chats cache while waiting for server response

--- src/app/services/chats.service.ts
+++ src/app/services/chats.service.ts
@@ -1,5 +1,5 @@
 import {ApolloQueryResult} from 'apollo-client';
-import {map} from 'rxjs/operators';
+import {concat, map} from 'rxjs/operators';
 import {Apollo, QueryRef} from 'apollo-angular';
 import {Injectable} from '@angular/core';
 import {getChatsQuery} from '../../graphql/getChats.query';
@@ -15,6 +15,8 @@ import {Observable} from 'rxjs/Observable';
 import {addChatMutation} from '../../graphql/addChat.mutation';
 import {addGroupMutation} from '../../graphql/addGroup.mutation';
 import * as moment from 'moment';
+import {AsyncSubject} from 'rxjs/AsyncSubject';
+import {of} from 'rxjs/observable/of';
 
 const currentUserId = '1';
 const currentUserName = 'Ethan Gonzalez';
@@ -24,6 +26,7 @@ export class ChatsService {
   getChatsWq: QueryRef<GetChats.Query>;
   chats$: Observable<GetChats.Chats[]>;
   chats: GetChats.Chats[];
+  getChatWqSubject: AsyncSubject<QueryRef<GetChat.Query>>;
 
   constructor(private apollo: Apollo) {
     this.getChatsWq = this.apollo.watchQuery<GetChats.Query>({
@@ -44,18 +47,32 @@ export class ChatsService {
   }
 
   getChat(chatId: string) {
+    const _chat = this.chats && this.chats.find(chat => chat.id === chatId) || null;
+    const chat$FromCache = of<GetChat.Chat>({
+      id: chatId,
+      name: this.chats ? _chat.name : '',
+      picture: this.chats ? _chat.picture : '',
+      isGroup: this.chats ? _chat.isGroup : false,
+      messages: this.chats && _chat.lastMessage ? [_chat.lastMessage] : [],
+    });
+
     const query = this.apollo.watchQuery<GetChat.Query>({
       query: getChatQuery,
       variables: {
-        chatId: chatId,
+        chatId,
       }
     });
 
-    const chat$ = query.valueChanges.pipe(
-      map((result: ApolloQueryResult<GetChat.Query>) => result.data.chat)
-    );
+    const chat$ = chat$FromCache.pipe(
+      concat(query.valueChanges.pipe(
+        map((result: ApolloQueryResult<GetChat.Query>) => result.data.chat)
+      )));
 
-    return {query, chat$};
+    this.getChatWqSubject = new AsyncSubject();
+    this.getChatWqSubject.next(query);
+    this.getChatWqSubject.complete();
+
+    return {query$: this.getChatWqSubject.asObservable(), chat$};
   }
 
   addMessage(chatId: string, content: string) {
