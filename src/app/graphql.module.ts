a99dca84c358757a46d16876ce732f805e9e5c41
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-12-23 14:43:01 +0100

    Step 3.1: Testing

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -5,16 +5,20 @@ import { InMemoryCache, defaultDataIdFromObject } from 'apollo-cache-inmemory';
 
 const uri = 'http://localhost:4000/graphql';
 
+export const dataIdFromObject = (object: any) => {
+  switch (object.__typename) {
+    case 'Message':
+      return `${object.chat.id}:${object.id}`; // use `chatId` prefix and `messageId` as the primary key
+    default:
+      return defaultDataIdFromObject(object); // fall back to default handling
+  }
+};
+
 export function createApollo(httpLink: HttpLink) {
   return {
-    link: httpLink.create({uri}),
+    link: httpLink.create({ uri }),
     cache: new InMemoryCache({
-      dataIdFromObject: (object: any) => {
-        switch (object.__typename) {
-          case 'Message': return `${object.chat.id}:${object.id}`; // use `chatId` prefix and `messageId` as the primary key
-          default: return defaultDataIdFromObject(object); // fall back to default handling
-        }
-      }
+      dataIdFromObject,
     }),
   };
 }
