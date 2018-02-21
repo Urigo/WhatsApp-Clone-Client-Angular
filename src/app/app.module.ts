3216e6d04edf3742a8802b3d8d101661a27e38de
Niccolò Belli <niccolo.belli@linuxsystems.it>
2018-02-21 16:44:50 +0100

    Step 10.1: Authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { GraphQLModule } from './graphql.module';
 import {ChatsListerModule} from './chats-lister/chats-lister.module';
 import {RouterModule, Routes} from '@angular/router';
 import {ChatViewerModule} from './chat-viewer/chat-viewer.module';
 import {ChatsCreationModule} from './chats-creation/chats-creation.module';
+import {LoginModule} from './login/login.module';
+import {AuthInterceptor} from './login/services/auth.interceptor';
 const routes: Routes = [];
 
 @NgModule({
@@ -24,8 +26,15 @@ const routes: Routes = [];
     ChatsListerModule,
     ChatViewerModule,
     ChatsCreationModule,
+    LoginModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
