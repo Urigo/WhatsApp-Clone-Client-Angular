edf2ab736f0af0a0a3bddfb42e575b509eb3ac67
Niccolò Belli <niccolo.belli@linuxsystems.it>
2018-02-26 17:45:58 +0100

    Step 1.4: Better normalize messages

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import { InMemoryCache, defaultDataIdFromObject } from 'apollo-cache-inmemory';
 
 const uri = 'http://localhost:4000/graphql';
 
 export function createApollo(httpLink: HttpLink) {
   return {
     link: httpLink.create({uri}),
-    cache: new InMemoryCache(),
+    cache: new InMemoryCache({
+      dataIdFromObject: (object: any) => {
+        switch (object.__typename) {
+          case 'Message': return `${object.chat.id}:${object.id}`; // use `chatId` prefix and `messageId` as the primary key
+          default: return defaultDataIdFromObject(object); // fall back to default handling
+        }
+      }
+    }),
   };
 }
 
