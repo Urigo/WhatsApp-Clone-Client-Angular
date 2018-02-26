8bf387d17d48f29a337975602da7c98f61e44c7d
Niccolò Belli <niccolo.belli@linuxsystems.it>
2018-02-26 17:45:58 +0100

    Step 1.4: Better normalize messages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,7 +37,14 @@ export class AppModule {
   ) {
     apollo.create({
       link: httpLink.create(<Options>{uri: 'http://localhost:3000/graphql'}),
-      cache: new InMemoryCache(),
+      cache: new InMemoryCache({
+        dataIdFromObject: (object: any) => {
+          switch (object.__typename) {
+            case 'Message': return `${object.chat.id}:${object.id}`; // use `chatId` prefix and `messageId` as the primary key
+            default: return defaultDataIdFromObject(object); // fall back to default handling
+          }
+        }
+      }),
     });
   }
 }
