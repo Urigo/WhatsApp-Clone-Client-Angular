a11170fdf4c110e0ba72760297b9fd2760e38600
Niccolò Belli <niccolo.belli@linuxsystems.it>
2019-01-13 21:57:30 +0100

    Step 12.2: Revert unfinished pagination

--- src/app/chats-lister/components/chat-item/chat-item.component.ts
+++ src/app/chats-lister/components/chat-item/chat-item.component.ts
@@ -8,7 +8,7 @@ import {GetChats} from '../../../../graphql';
       <img class="chat-pic" [src]="chat.picture || 'assets/default-profile-pic.jpg'">
       <div class="chat-info">
         <div class="chat-name">{{ chat.name }}</div>
-        <div class="chat-last-message">{{ chat.messageFeed.messages[chat.messageFeed.messages.length - 1]?.content }}</div>
+        <div class="chat-last-message">{{ chat.messages[chat.messages.length - 1]?.content }}</div>
         <div class="chat-timestamp">00:00</div>
       </div>
     </div>
